ac293ceaf73f542365b13a06d24645b4dd05b28e
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-08-29 16:54:33 +0100

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,7 @@ const Nav = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={logo} alt="Logo Image" style={{ width: "50px" }} />
+        <i src={logo} alt="Logo Image" style={{ width: "50px" }} />
       </div>
 
       {/* menu */}
