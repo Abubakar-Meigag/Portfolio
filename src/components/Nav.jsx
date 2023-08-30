a8e8cd13e12db918124268ea184ca37da13affbb
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-08-30 10:21:10 +0100

    update navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,27 +16,27 @@ const Nav = () => {
 
       {/* menu */}
       <ul className="hidden md:flex">
-        <li>
+        <li className="hover:text-pink-600 font-bold">
           <Link to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li>
+        <li className="hover:text-pink-600 font-bold">
           <Link to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li>
+        <li className="hover:text-pink-600 font-bold">
           <Link to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li>
+        <li className="hover:text-pink-600 font-bold">
           <Link to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li>
+        <li className="hover:text-pink-600 font-bold">
           <Link to="contact" smooth={true} duration={500}>
             Contact
           </Link>
@@ -56,27 +56,27 @@ const Nav = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">
+        <li className="py-6  hover:text-pink-600 font-bold text-4xl">
           <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 hover:text-pink-600 font-bold text-4xl">
           <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 hover:text-pink-600 font-bold text-4xl">
           <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 hover:text-pink-600 font-bold text-4xl">
           <Link onClick={handleClick} to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 hover:text-pink-600 font-bold text-4xl">
           <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
             Contact
           </Link>
