9375ea20041d670d16a66a5a77a5445bca2fd9d0
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2024-01-07 21:47:46 +0000

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
     >
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          <div className="sm:text-right pb-8 pl-4">
+          <div className="sm:text-right pb-5 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-pink-600">
               About
             </p>
@@ -31,34 +31,38 @@ const About = () => {
             data-aos="fade-right"
           >
             <p>
-              Thanks to visit my Portfolio. Please check my work and contact
-              with me via below contact Box or by my Email
+              Thank you for taking the time to view my portfolio. Please feel
+              free to review my work and contact me using the form below or via
+              email
             </p>
             <br />
             <p className="sm:text-right text-1xl font-bold">Thank you</p>
           </div>
-          <div data-aos="fade-left" className="text-[#d5d7e1]">
-            <p>
+          <div data-aos="fade-left" className="text-[#caccd7]">
+            <p className="text-[15px]">
               As a person, to lead gives me personal fulfilment, I like to learn
               from others by being a good listener, I easily adapt and I have
               great confidence in my abilities. In my profession, I have gained
               more than{" "}
               <strong className="text-[#d4e0e5]">15 years of experience</strong>{" "}
-              in customer service and administrative work, I have led teams to
-              achieve the goals and vision of the management. I have developed
-              great skills in team building, effective communication, task
-              distribution, time management, critical thinking, conflict
-              resolution and cooperation.
+              in customer service and administrative work, I have experience
+              leading teams and developing skills in team building,
+              communication, task distribution, time management, critical
+              thinking, conflict resolution, and cooperation.
               <br />
               <br />
               My greatest dream and now passion is to learn and develop as a
               coder. My love for coding fuels my patience and determination to
               master it. I found an amazing opportunity at the coding program
               CodeYourFuture, where I learned coding and built up a strong
-              foundation to be successful in my profession. The curriculum was
-              well planned and I got excellent guidance from the teaching team.
-              It made me a Full Stack developer, eager to learn from my team
-              mates and leverage my skills in significant projects
+              foundation to be successful in my profession.
+              <br />
+              <br />
+              My goal is to bring our shared vision to fruition by creating
+              something that provides an exceptional user experience. Whether
+              you share my enthusiasm, are interested in collaborating, or are
+              simply curious about the world of web development, I am thrilled
+              to have you here.
             </p>
           </div>
         </div>
