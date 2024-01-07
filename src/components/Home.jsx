cf321e70e34637b6dd092fa43fbed01a4d7f22b7
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2024-01-07 19:50:42 +0000

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { HiArrowNarrowRight } from "react-icons/hi"
+import { TypeAnimation } from "react-type-animation";
 import { Link } from "react-scroll";
 
+
+
 const Home = () => {
+
 return (
   <div name="home" className="w-full h-screen bg-[#0a192f]">
     <div className="max-w-[1000px] mx-auto flex flex-col justify-center h-full">
@@ -11,19 +15,34 @@ return (
         Abubakar Meigag
       </h1>
       <h2 className="text-3xl sm:text-3xl font-bold text-[#d4e0e5]">
-        I'm Full-Stack Developer
+        <span className="font-bold">
+          <TypeAnimation
+            sequence={[
+              "Software Engineer.",
+              2000,
+              "Frontend Developer.",
+              2000,
+              "Backend Developer.",
+              2000,
+            ]}
+            wrapper="div"
+            cursorStyle={true}
+            repeat={Infinity}
+            style={{ fontSize: "1em", paddingLeft: "5px" }}
+          />
+        </span>
       </h2>
       <p className="text-[#afb1bb] py-4 max-w-[700px]">
         I achieved my dream of becoming a Full-Stack Developer after completing
         the CodeYourFuture program. Proficient in{" "}
         <strong className="text-[#d4e0e5]">
           {" "}
-          HTML, CSS, JavaScript, React, Node, PostgreSQL, MongoDB, AWS
+          HTML, CSS, JavaScript, React.js, Node.js, PostgreSQL, MongoDB, AWS
           technologies{" "}
         </strong>
         , and <strong className="text-[#d4e0e5]">Agile</strong> work practice
         experience. Successfully led a team in a final project incorporating
-        various technologies, including (Supabase) for databases. Possess over
+        various technologies, including (supabase) for databases. Possess over
         <strong className="text-[#d4e0e5]"> 15 years</strong> of leadership
         experience in customer service and administration in the retail
         industry. A versatile professional with strong technical skills and a
