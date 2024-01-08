2baeef430229ae7d95ee91ebe9f44e768f8a25da
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2024-01-08 22:11:58 +0000

    update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -15,7 +15,10 @@ const Work = () => {
 
 
   return (
-    <div name="work" className="w-full pt-8 md:h-screen text-gray-300 bg-[#0a192f]">
+    <div
+      name="work"
+      className="w-full pt-8 md:h-screen text-gray-300 bg-[#0a192f]"
+    >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-2">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
@@ -41,14 +44,18 @@ const Work = () => {
                 <span className="text-white tracking-wider ">{item.name}</span>
 
                 <div className="flex justify-center pt-4 relative">
-                  <a href={item.github}>
+                  <a
+                    href={item.github}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <button className="flex justify-center items-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-md hover:bg-pink-600 hover:text-white">
                       <FaGithub className="mr-1" />
                       Code Source
                     </button>
                   </a>
 
-                  <a href={item.live}>
+                  <a href={item.live} rel="noopener noreferrer" target="_blank">
                     <button className="flex justify-center items-center rounded-lg px-2 py-2 m-2 bg-white text-gray-700 font-bold text-md hover:bg-pink-600 hover:text-white">
                       <TbExternalLink className="mr-1" />
                       Live Demo
