af7ce42b3633011eee91eb8c915f407d744837c3
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-08-05 01:47:34 +0100

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -48,14 +48,14 @@ return (
         and conflict resolution.
       </p>
       <div>
+      <Link to="work" smooth={true} duration={500} className='cursor-pointer'>
         <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-          <Link to="work" smooth={true} duration={500}>
             View MY Work
-          </Link>
           <span className="group-hover:rotate-90 duration-300">
             <HiArrowNarrowRight className="ml-3" />
           </span>
         </button>
+        </Link>
       </div>
     </div>
   </div>
